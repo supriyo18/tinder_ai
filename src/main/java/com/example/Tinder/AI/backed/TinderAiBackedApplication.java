8ce3f657e625688876585a1f4cf7ff7e98577ffe
supriyo18 <mukherjee.supriyo18@gmail.com>
2024-08-07 01:59:28 +0530

    push the first version

---
+++ src/main/java/com/example/Tinder/AI/backed/TinderAiBackedApplication.java
@@ -0,0 +1,17 @@
+package com.example.Tinder.AI.backed;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TinderAiBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiBackedApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/example/Tinder/AI/backed/TinderAiBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tinder.AI.backed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
